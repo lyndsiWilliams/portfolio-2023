7dca408ca1ae3536dbd049a28718bd829838e1a0
Lyndsi Kay Williams <kcatgirl@gmail.com>
2023-02-24 09:18:11 -0600

    Fix resume page length

--- portfolio/src/components/ResumePage.tsx
+++ portfolio/src/components/ResumePage.tsx
@@ -5,7 +5,7 @@ const ResumePage = () => {
         src="https://docs.google.com/document/d/e/2PACX-1vRERMPCJxpPmykMYDJBhpiWHkq2_h-846yIRP1Ivti76hCfus1pHb-azUg96dGlVGmxr-h4c6VdFp8i/pub?embedded=true"
         title="file"
         width="80%"
-        height="620px"
+        height="1350px"
         style={{ maxWidth: "834px" }}
       ></iframe>
     </div>
