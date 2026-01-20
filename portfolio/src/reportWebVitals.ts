98d3bd6486fdabf865e9726591211d6bfd5e3ff1
Lyndsi <kcatgirl@gmail.com>
2026-01-20 00:19:34 -0600

    update dependencies

--- portfolio/src/reportWebVitals.ts
+++ portfolio/src/reportWebVitals.ts
@@ -1,15 +1,16 @@
-import { ReportHandler } from 'web-vitals';
+import type { Metric } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+const reportWebVitals = (onPerfEntry?: (metric: Metric) => void) => {
+  if (!onPerfEntry) return;
+
+  import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB, onINP }) => {
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
+    if (onINP) onINP(onPerfEntry);
+  });
 };
 
 export default reportWebVitals;
