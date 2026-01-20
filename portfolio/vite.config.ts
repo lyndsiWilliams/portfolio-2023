79265a1030060c41f10792132d5af5442463f4a3
Lyndsi <kcatgirl@gmail.com>
2026-01-20 00:05:00 -0600

    Migrate from CRA to Vite

---
+++ portfolio/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@images': path.resolve(__dirname, './src/images'),
+      '@downloadableContent': path.resolve(__dirname, './src/downloadableContent'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+  },
+})
